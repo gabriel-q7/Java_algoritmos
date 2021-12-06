9be6b19a5441a9c1903f21d068c58b9814273961
gabriel <gabriel_aq7@hotmail.com>
2021-12-06 14:54:40 -0300

    update bubble sort

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -1,6 +1,21 @@
 
 public class bubbleSort {
     public static void main(String[] args) {
-        System.out.println("teste");
+
+        int[] vetor = {2,23,54,1,71,2,59};
+
+        for(int i=0; i< vetor.length - 2; i++){
+            for(int j=0; j < vetor.length - 2 - i; j++){
+                if(vetor[j] > vetor[j+1]){
+                    int temp;
+                    temp = vetor[j+1];
+                    vetor[j+1] = vetor[j];
+                    vetor[j] = temp;
+                }
+            }
+        }
+        for(int numero: vetor){
+            System.out.println(numero);
+        }
     }
 }
