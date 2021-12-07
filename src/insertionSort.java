36583d4ede3d8c70510eb516c2d93400c8f42dc6
gabriel <gabriel_aq7@hotmail.com>
2021-12-07 15:12:12 -0300

    update insertionS

--- src/insertionSort.java
+++ src/insertionSort.java
@@ -1,13 +1,17 @@
-//
+// Insertion sort: O(n^2) time complexity
+
 public class insertionSort {
+
     public static void main(String[] args) {
+
         int[] arrayNumeros = {2,23,41,66,1,3};
 
         for(int i = 0; i < arrayNumeros.length; i++){
             int atual = arrayNumeros[i];
             int j = i - 1;
 
-            while(j >= 0 && arrayNumeros[j] >= atual){
+            //_____________________________ > stable; >= unstable
+            while(j >= 0 && arrayNumeros[j] > atual){
                 arrayNumeros[j+1] = arrayNumeros[j];
                 j--;
             }
