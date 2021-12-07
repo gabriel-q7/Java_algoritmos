f8d41e96515542c1a30eaca985173705d701c10e
gabriel <gabriel_aq7@hotmail.com>
2021-12-07 14:13:51 -0300

    update insertionS

--- src/insertionSort.java
+++ src/insertionSort.java
@@ -1,6 +1,21 @@
 //
 public class insertionSort {
     public static void main(String[] args) {
+        int[] arrayNumeros = {2,23,41,66,1,3};
 
+        for(int i = 0; i < arrayNumeros.length; i++){
+            int atual = arrayNumeros[i];
+            int j = i - 1;
+
+            while(j >= 0 && arrayNumeros[j] >= atual){
+                arrayNumeros[j+1] = arrayNumeros[j];
+                j--;
+            }
+            arrayNumeros[j+1] = atual;
+        }
+
+        for(int numero: arrayNumeros){
+            System.out.println(numero);
+        }
     }
 }
