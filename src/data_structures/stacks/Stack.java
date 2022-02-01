085076431e7aef9843deef37a5c8d703dfadbbff
gabriel <gabriel_aq7@hotmail.com>
2022-02-01 14:41:00 -0300

    stack creation

---
+++ src/data_structures/stacks/Stack.java
@@ -0,0 +1,11 @@
+package data_structures.stacks;
+//push O(1)
+//pop O(1)
+//peek O(1)
+public class Stack {
+    int maxSize = 9;
+    int[] stackArray = new int[maxSize];
+
+    int top = -1;
+
+}
